61d274bec7f1597ab58a983f01c3e954d9d3a6f0
Kenny <101839505+khui0@users.noreply.github.com>
2022-06-30 14:51:18 -0400

    js

--- main.js
+++ main.js
@@ -4,27 +4,62 @@ const feed = document.getElementById("feed");
 var data = {};
 
 document.querySelectorAll("[data-open]").forEach(item => {
-    item.addEventListener("click", e => {
+    item.addEventListener("click", () => {
         document.getElementById(item.getAttribute("data-open")).showModal();
-    })
-})
+    });
+});
 
 document.querySelectorAll("[data-close]").forEach(item => {
-    item.addEventListener("click", e => {
+    item.addEventListener("click", () => {
         document.getElementById(item.getAttribute("data-close")).close();
-    })
-})
+    });
+});
 
-function createTask(title, date, details) {
-    feed.innerHTML += `<div class="card" id="${title}">
+document.getElementById("task-submit").addEventListener("click", () => {
+    let title = document.getElementById("task-title").value;
+    let date = document.getElementById("task-date").value;
+    let details = document.getElementById("task-details").value;
+
+    let timestamp = new Date(date).getTime();
+    if (title?.trim() && date?.trim() && details?.trim()) {
+        let uuid = uuidv4();
+        createTask(uuid, title, new Date(timestamp).toLocaleString(), details);
+        // Close modal and reset inputs
+        document.getElementById("task").close();
+        document.getElementById("task-title").value = "";
+        document.getElementById("task-date").value = "";
+        document.getElementById("task-details").value = "";
+    }
+    else {
+        alert("Fields cannot be left blank");
+    }
+});
+
+function createTask(uuid, title, date, details) {
+    feed.innerHTML += `<div class="card" id="${uuid}">
         <h3>${title}</h3>
         <p>${date}</p>
         <p>${details}</p>
         <div class="button-cluster">
-            <button>Edit</button>
-            <button>Finished</button>
+            <button data-edit="${uuid}">Edit</button>
+            <button data-delete="${uuid}">Finished</button>
         </div>
     </div>`;
+    addButtonClick();
+}
+
+function addButtonClick() {
+    document.querySelectorAll("[data-edit]").forEach(item => {
+        item.addEventListener("click", () => {
+            let uuid = item.getAttribute("data-edit");
+            console.log("edit task with id: " + uuid);
+        });
+    });
+    document.querySelectorAll("[data-delete]").forEach(item => {
+        item.addEventListener("click", () => {
+            document.getElementById(item.getAttribute("data-delete")).remove();
+        });
+    });
 }
 
 // let uuid = uuidv4();
