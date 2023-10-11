b339477841f1879cd17366c0e645cefc693f460f
Kenny <101839505+khui0@users.noreply.github.com>
2023-10-10 20:28:34 -0400

    Fix layout

--- src/archive.js
+++ src/archive.js
@@ -2,13 +2,13 @@ import * as time from "./time.js";
 import * as ui from "./ui.js";
 
 document.getElementById("archive").addEventListener("click", e => {
-    updateArchive();
     ui.show(document.getElementById("archive-modal"), "Archive", [
         {
             text: "Close",
             close: true,
         },
     ]);
+    updateArchive();
 });
 
 function updateArchive() {
