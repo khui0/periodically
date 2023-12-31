c722e0a7fdf7f38c3fc15b34f3f3a6b99b2f0e5b
Kenny <101839505+khui0@users.noreply.github.com>
2023-12-31 15:42:10 -0500

    Customizable font weight and detail lines

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,19 +1,21 @@
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateTheme);
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateColors);
 document.addEventListener("settingchange", update);
 
 update();
 
 function update() {
     disableTransitions();
-    updateTheme();
+    updateColors();
     updateBackground();
+    updateFont();
+    updateDetails();
     enableTransitions();
 }
 
-function updateTheme() {
-    const theme = localStorage.getItem("periodically-theme");
+function updateColors() {
+    const setting = localStorage.getItem("periodically-theme");
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    switch (theme) {
+    switch (setting) {
         case "light": {
             document.body.classList.remove("theme-dark");
             break;
@@ -29,17 +31,49 @@ function updateTheme() {
 }
 
 function updateBackground() {
-    const style = localStorage.getItem("periodically-background") || "default";
+    const setting = localStorage.getItem("periodically-background") || "default";
     document.querySelectorAll(".list").forEach(list => {
         list.classList.remove("fill");
         list.classList.remove("outline");
-        switch (style) {
+        switch (setting) {
             case "fill": {
                 list.classList.add("fill");
                 break;
             }
             case "outline": {
                 list.classList.add("outline");
+                break;
+            }
+        }
+    });
+}
+
+function updateFont() {
+    const setting = localStorage.getItem("periodically-font-weight") || "normal";
+    document.querySelectorAll(".list").forEach(list => {
+        switch (setting) {
+            case "normal": {
+                list.classList.remove("bold");
+                break;
+            }
+            case "bold": {
+                list.classList.add("bold");
+                break;
+            }
+        }
+    });
+}
+
+function updateDetails() {
+    const setting = localStorage.getItem("periodically-details") || "default";
+    document.querySelectorAll(".list").forEach(list => {
+        switch (setting) {
+            case "default": {
+                list.classList.remove("multiline");
+                break;
+            }
+            case "multiline": {
+                list.classList.add("multiline");
                 break;
             }
         }
