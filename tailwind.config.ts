a4e12e5a118ec9a3c572dafaf596d0c47737c0c0
Kenny <101839505+khui0@users.noreply.github.com>
2024-10-30 20:26:09 -0400

    feat: prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import containerQueries from '@tailwindcss/container-queries';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import containerQueries from "@tailwindcss/container-queries";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {},
+  },
 
-	plugins: [typography, forms, containerQueries]
+  plugins: [typography, forms, containerQueries],
 } satisfies Config;
