7784ab4b0adb28de98f6f4c89d09a2af280f15bc
Kenny <101839505+khui0@users.noreply.github.com>
2024-10-31 02:08:32 -0400

    feat: homepage scaffolding

---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+import "../app.css";
+import "$lib/theme";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header class="sticky top-0 border-b border-base-200 p-5">
+  <div class="mx-auto flex max-w-[65ch] justify-between">
+    <div class="flex flex-col gap-0.5">
+      <h1 class="text-3xl font-bold leading-none tracking-tight">Periodically 5</h1>
+      <p class="mx-0.5 leading-none prose"><i>Insert copywriting here</i></p>
+    </div>
+    <div class="flex items-center gap-2.5">
+      <a href="/" class="btn">Sign in</a>
+      <a href="/" class="btn btn-accent">Register</a>
+    </div>
+  </div>
+</header>
+<main class="p-5">
+  <div class="prose mx-auto">
+    <h2>Lorem ipsum dolor sit amet.</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo cumque officia architecto in,
+      mollitia perferendis possimus aperiam earum eligendi sapiente?
+    </p>
+    <h2>Lorem, ipsum dolor.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, magni!</p>
+    <h2>Lorem ipsum dolor sit.</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos aliquid repellendus illum
+      placeat totam? Sed at laborum alias earum dolore nisi unde!
+    </p>
+  </div>
+</main>
