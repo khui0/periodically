8997ac53bb425551582af56b573a406ea4d8fd3d
Kenny <101839505+khui0@users.noreply.github.com>
2024-10-30 20:25:29 -0400

    Revert "feat: add shadcn-svelte"
    This reverts commit 796293693dc83415833ea8828c0189fa5d1a12f8.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,2 @@
-<script lang="ts">
-  import Button from "$lib/components/ui/button/button.svelte";
-</script>
-
-<Button>Hi</Button>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
