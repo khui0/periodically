796293693dc83415833ea8828c0189fa5d1a12f8
Kenny <101839505+khui0@users.noreply.github.com>
2024-10-30 20:06:27 -0400

    feat: add shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<Button>Hi</Button>
